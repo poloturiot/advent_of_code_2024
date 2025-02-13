3defef2d74060d5ae6a991190a06aea4cf853d77
Paul Thuriot <paul.thuriot1@gmail.com>
2025-02-13 21:27:39 +0100

    part2 OK

--- day3/script.py
+++ day3/script.py
@@ -2,8 +2,30 @@ import re
 
 txt = open("input.txt").read()
 
-matches = re.findall("(?<=mul\()(\d{1,3}),(\d{1,3})(?=\))", txt)
+matches = re.findall("(?<=mul\()\d{1,3},\d{1,3}(?=\))|do\(\)|don't\(\)", txt)
 
-result = [int(match[0])*int(match[1]) for match in matches]
+# print(matches)
 
-print(sum(result))
+enabled = True
+
+results=[]
+
+for match in matches:
+    # print(enabled)
+    if match == "don't()":
+        enabled = False
+    elif match == "do()":
+        enabled = True
+    elif enabled:
+        args = [int(i) for i in match.split(",")]
+        result = args[0] * args[1]
+        results.append(result)
+        # print(match + "=" + str(result))
+    # else :
+        # print(match, "Not enabled")
+
+print(sum(results))
+
+# result = [int(match[0])*int(match[1]) for match in matches]
+
+# print(sum(result))
